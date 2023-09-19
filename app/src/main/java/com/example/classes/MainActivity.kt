393c8f01f53e7a9cf75cfc8f2a9f9b45d84fbdf9
lilovy <43091143+lilovy@users.noreply.github.com>
2023-09-19 17:36:52 +0500

    update code

--- app/src/main/java/com/example/classes/MainActivity.kt
+++ app/src/main/java/com/example/classes/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.classes
 
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnClickListener
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.fathzer.soft.javaluator.DoubleEvaluator
 
 
 class MainActivity: AppCompatActivity(), View.OnClickListener {
@@ -12,8 +12,40 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(v: View?) {
         var tv = v as TextView
+        var resultTV = findViewById<TextView>(R.id.resultTV)
+        var oldText = resultTV.text.toString()
+
+        when(tv.text.toString()){
+            "Del" -> {
+                if (oldText.length > 0){
+                    var newText = oldText.substring(0, oldText.length - 1)
+                    resultTV.setText(newText)
+                }
+            }
+            "AC" -> {resultTV.setText(null)}
+            "=" -> {
+                var evaluator = DoubleEvaluator()
+                var expression = resultTV.text.replace(Regex("×"), "*")
+                var result = evaluator.evaluate(expression)
+                resultTV.setText(result.toString())
+            }
+            else -> {
+                var toAppendString = tv.text.toString()
+                if(isOperator(toAppendString[0]) && isOperator(oldText[oldText.length - 1])){
+                    oldText = oldText.substring(0, oldText.length - 1)
+                }
+                var newText = oldText + toAppendString
+                resultTV.setText(newText)
+            }
+        }
+    }
+
+    public fun isOperator(c: Char): Boolean {
+        when(c){
+            'x', '×', '*', '/', '+', '-' -> { return true}
+            else -> return false
+        }
     }
 }
